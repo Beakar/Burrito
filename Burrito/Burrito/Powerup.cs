ec0da3160714a5b672badcf6ab718b18cdef747b
christiw16 <willchristiansen7@gmail.com>
2014-01-30 13:43:41 -0800

    Start of powerup classes, no where near working yet

---
+++ Burrito/Burrito/Powerup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Burrito
+{
+    abstract class Powerup
+    {
+       protected int x;
+       protected int y;
+
+        public Powerup(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Draw(SpriteBatch spritebatch);
+        
+
+
+    }
+}
---
+++ Burrito/Burrito/SpeedPowerUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Burrito
+{
+    class SpeedPowerUp: Powerup
+    {
+        Texture2D myTexture;
+        public SpeedPowerUp(int x, int y) : base(x, y)
+        {
+            
+        }
+        
+        public void Draw(SpriteBatch spriteBatch){
+                   spriteBatch.Draw(myTexture, new Rectangle(base.x, base.y, 30, 30),//powerups are 30 by 30
+                Color.White);
+        }
+    }
+}
