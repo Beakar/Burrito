92340f1b32be561e574ed0e3332dd9f9089c52e5
nateyb93 <brownn15@up.edu>
2014-02-03 23:14:00 -0800

    fix speedpowerup class

--- Burrito/Burrito/SpeedPowerUp.cs
+++ Burrito/Burrito/SpeedPowerUp.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,34 +8,16 @@ using Microsoft.Xna.Framework.Content;
 
 namespace Burrito
 {
-    public class SpeedPowerUp : PowerUp
+    class SpeedPowerUp: PowerUp
     {
         Texture2D myTexture;
-<<<<<<< HEAD
-        public SpeedPowerUp(int x, int y)
-        {
-<<<<<<< HEAD
-          
-        }
-      
-=======
-
-        }
-
-        public void Draw(SpriteBatch spriteBatch)
-        {
-            spriteBatch.Draw(myTexture, new Rectangle(base.position.X, base.position.Y, 30, 30),//powerups are 30 by 30
-         Color.White);
-=======
         public SpeedPowerUp(int x, int y, Texture2D texture)
             : base(x, y, texture)
         { }
         
         
->>>>>>> 63cdd92f8c3418623bafd3bef48e144d0cb85f44
         public void Draw(SpriteBatch spriteBatch){
             base.Draw(spriteBatch);
->>>>>>> d671f47dac8051fac340d0cbd9257ec471c2f04d
         }
     }
 }
