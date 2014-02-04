b0809cff8cae7744a9e916dd93cfd12647bcf0e6
Benjamin Rumptz <rumptz15@up.edu>
2014-02-04 08:40:56 -0800

    Obstacle Update 5 - Hitboxes
    Early alpha hitboxes are present

--- Burrito/Burrito/Obstacle.cs
+++ Burrito/Burrito/Obstacle.cs
@@ -13,15 +13,18 @@ namespace Burrito
 {
     class Obstacle
     {
-        protected Texture2D texture;  //The texture that this obstacle uses
-        protected Vector2 position;   //The postion that the obstacle will be drawn (x,y)
-        protected Vector2 size;       //The Size of the obstacle (width,height)
+        protected Texture2D texture;  //The texture that this Obstacle uses
+        protected Vector2 position;   //The postion that the Obstacle will be drawn (x,y)
+        protected Vector2 size;       //The Size of the Obstacle (width,height)
+        protected Rectangle hitbox;   //The Obstacle's hitbox
 
         public Obstacle(Texture2D newTexture, Vector2 newPos)
         {
             texture = newTexture;
             position = newPos;
             size = new Vector2(texture.Width, texture.Height);
+            //The hitbox is a rectangle 3/4 the size of the texture
+            hitbox = new Rectangle((int)(position.X+(size.X*.25)), (int)(position.Y+(size.Y*.25)), (int)(size.X*.75), (int)(size.Y*.75));
         }
 
         public void Draw(SpriteBatch sb)
@@ -35,10 +38,17 @@ namespace Burrito
         }
 
         //UPDATE
-        //Moves the obstacle to the left deltaX length
+        //Moves the obstacle to the left a given (deltaX) length
         public void Update(float deltaX)
         {
             position.X -= deltaX;
+        }
+
+        //Creates a hitbox for our Obstacle
+        //Checks if the hitbox was touched
+        public bool wasHit()
+        {
+            return false;
         }
     }
 }
