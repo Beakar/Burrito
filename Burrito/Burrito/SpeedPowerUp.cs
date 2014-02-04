6060ecae20b31eb87e6ba04657bb8a7ba476a7f1
Benjamin Rumptz <rumptz15@up.edu>
2014-02-03 21:34:42 -0800

    Prepush commit

--- Burrito/Burrito/SpeedPowerUp.cs
+++ Burrito/Burrito/SpeedPowerUp.cs
@@ -8,17 +8,34 @@ using Microsoft.Xna.Framework.Content;
 
 namespace Burrito
 {
-    class SpeedPowerUp: PowerUp
+    public class SpeedPowerUp : PowerUp
     {
         Texture2D myTexture;
-        public SpeedPowerUp(int x, int y) : base(x, y)
+<<<<<<< HEAD
+        public SpeedPowerUp(int x, int y)
         {
+<<<<<<< HEAD
           
         }
       
+=======
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(myTexture, new Rectangle(base.position.X, base.position.Y, 30, 30),//powerups are 30 by 30
+         Color.White);
+=======
+        public SpeedPowerUp(int x, int y, Texture2D texture)
+            : base(x, y, texture)
+        { }
+        
+        
+>>>>>>> 63cdd92f8c3418623bafd3bef48e144d0cb85f44
         public void Draw(SpriteBatch spriteBatch){
-                   spriteBatch.Draw(myTexture, new Rectangle(base.x, base.y, 30, 30),//powerups are 30 by 30
-                Color.White);
+            base.Draw(spriteBatch);
+>>>>>>> d671f47dac8051fac340d0cbd9257ec471c2f04d
         }
     }
 }
